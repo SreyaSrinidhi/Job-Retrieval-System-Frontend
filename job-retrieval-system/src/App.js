964ee067bb18b7217d51de27e72c98b1b69b098c
Sreya-Srinidhi <sreyaksrinidhi@gmail.com>
2026-02-15 16:57:43 -0500

    initialized react app

---
+++ job-retrieval-system/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ResumeUploadPage from './resumeUploadPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeUploadPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ job-retrieval-system/src/resumeUploadPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "@mui/material/Container"
+
+function ResumeUploadPage() {
+    return (
+        <div className="header">
+            <h1>Job Retrieval Platform</h1>
+        </div>
+    )
+};
+
+function ResumeUpload(){
+    <Container>
+        Upload Resume Here
+    </Container>
+};
+
+export default ResumeUploadPage;
